1143e638585a3d859b1b99ba5f8f19ebc452422d
GermanRivera15 <griveraol@unprg.edu.pe>
2026-01-26 11:07:13 -0500

    Update User Api Tesis

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class User extends Authenticatable
+class User extends Model
 {
     use HasFactory;
     protected $table = 'user';
